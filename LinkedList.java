bc7cc622d172dfcdd31a0d6cead7640f1fd7233c
LittleBugEmpire <littlebugempire@gmail.com>
2023-07-22 12:41:51 +0530

    Linked List methods

--- LinkedList.java
+++ LinkedList.java
@@ -3,7 +3,6 @@ public class LinkedList {
     private Node head;
     private Node tail;
     private int length;
-
     public LinkedList(int value){
         Node newNode = new Node(value);
         head = newNode;
